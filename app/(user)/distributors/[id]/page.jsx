10363004851d5d09f5a9af7aabd2c69227f1898d
Christian Ezeanyaeche <christianezeanyaeche@gmail.com>
2023-06-25 17:33:02 +0100

    chore: updated distributors pages

--- app/(user)/distributors/[id]/page.jsx
+++ app/(user)/distributors/[id]/page.jsx
@@ -1,14 +1,36 @@
-'use client'
-import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@lib/auth'
+import Link from 'next/link'
+import { DateUI } from '@app/(user)/Date'
 
-const Page = ({ params }) => {
-    const router = useRouter()
-    const id = params.id
+const Page = async ({ params }) => {
+    const session = await getServerSession(authOptions);
+    const user = session.user;
+    if (!user) {
+        return
+    }
+    const distributorId = params.id
+
+    const res = await fetch(process.env.API_BASE_URL + '/users/' + user?.id + '/distributors/' + distributorId, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${session?.accessToken}`
+        }
+    })
+    const data = await res.json()
+    const distributor = data.data
+    // const router = useRouter()
 
     return (
         <div className='p-4 min-h-[calc(100dvh-56px)] w-full desktop:px-8'>
-            <button onClick={() => { router.back() }} className='bg-green-800 text-white py-2 px-8 rounded-sm block mb-4'>Back</button>
-            page yet to be implemented
+            <header className='flex py-4 justify-between items-center'>
+                <Link href='/distributors' className='bg-green-800 text-white py-2 px-8 rounded-md'>Back</Link>
+                <div>
+                    <DateUI />
+                </div>
+            </header>
+            <h1 className='font-bold text-2xl'>{distributor?.organization}</h1>
         </div>
     )
 }
--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -3,7 +3,7 @@ import { LeftSideBar, RightSideBar } from '../SideBar/SideBar'
 
 export const Home = ({ isAdmin, children }) => {
     return (
-        <div className='bg-[#f1f1f1] desktop:flex desktop:justify-between w-[100dvw]'>
+        <div className='bg-[#f1f1f1] desktop:flex desktop:justify-between w-screen'>
             <LeftSideBar isAdmin={isAdmin} />
             {children}
             <RightSideBar />
